663441553153f27bfaf7e127fce4753ad99495ca
Cameron Sutter <csutter@instructure.com>
2013-10-09 14:40:57 -0600

    Added a few enemies you can destroy

---
+++ corneria/Assets/destroyOnHit.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnCollisionEnter(col : Collision){
+	Debug.Log(gameObject.name);
+	Destroy (gameObject, 1.5f);
+	Destroy (col.gameObject);
+}
