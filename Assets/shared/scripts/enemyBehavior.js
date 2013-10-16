f810affad9addde97b066b0ad0eb678e1f1b36f1
Cameron Sutter <csutter@instructure.com>
2013-10-16 13:06:56 -0600

    Improved enemy behavior

--- Assets/shared/scripts/enemyBehavior.js
+++ Assets/shared/scripts/enemyBehavior.js
@@ -1,25 +1,46 @@
 ﻿#pragma strict
 
+public var projectile : Rigidbody;
+public var shootFrequency : float;
+//The middle component of the enemy
+public var middle : Transform;
+//The tallest component of the enemy
+public var height : Transform;
+
 private var attacking : boolean = false;
 private var player : GameObject = null;
 
 function OnCollisionEnter(col : Collision){
-	Destroy (gameObject, 1.5f);
-	Destroy (col.gameObject);
-	attacking = false;
+	if(col.gameObject.name == "Laser(Clone)"){
+		Destroy (gameObject, 1.5f);
+		Destroy (col.gameObject);
+		attacking = false;
+	}
 }
 
 function attack(toAttack : GameObject){
 	attacking = true;
 	player = toAttack;
+	fire();
 }
 
 function Update(){
-	if (attacking && player != null){
+	if (attacking && player){
 		transform.LookAt(player.transform);
 		//Lerp to a position just infront of the player
-		var newposition = player.transform.position;
+		var newposition : Vector3 = player.transform.position;
 		newposition.z = player.transform.position.z + 100;
+		newposition.y = player.transform.position.y - (height.renderer.bounds.size.y / 2);
 		transform.position = Vector3.Lerp(transform.position, newposition, Time.deltaTime);
+	}
+}
+
+function fire(){
+	while(attacking){
+		var projectileInstance : Rigidbody = Instantiate(projectile, middle.position, transform.rotation);
+		if(player){
+			projectileInstance.SendMessage("fire", player);
+		}
+		yield WaitForSeconds(shootFrequency);
 	}
 }
---
+++ Assets/shared/scripts/plasma_behavior.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+private var target : GameObject = null;
+private var turnSpeed : float = 1000;
+
+function Start(){
+	Destroy (gameObject, 1.5f);
+}
+
+function Update () {
+	transform.Rotate(Vector3.up + Vector3.right, turnSpeed * Time.deltaTime);
+	if(target){
+		transform.position = Vector3.Lerp(transform.position, target.transform.position, Time.deltaTime);
+	}
+}
+
+function fire(toTarget : GameObject){
+	target = toTarget;
+}
