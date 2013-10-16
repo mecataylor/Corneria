3b0939b813ba617fb82647a58ba2a2e077e7318f
Cameron Sutter <cameronsutter0@gmail.com>
2013-10-15 23:09:33 -0600

    enemy behavior and proximity sphere

--- Assets/shared/scripts/destroyOnHit.js
+++
@@ -1,6 +0,0 @@
-﻿#pragma strict
-
-function OnCollisionEnter(col : Collision){
-	Destroy (gameObject, 1.5f);
-	Destroy (col.gameObject);
-}
---
+++ Assets/shared/scripts/enemyBehavior.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+private var attacking : boolean = false;
+private var player : GameObject = null;
+
+function OnCollisionEnter(col : Collision){
+	Destroy (gameObject, 1.5f);
+	Destroy (col.gameObject);
+	attacking = false;
+}
+
+function attack(toAttack : GameObject){
+	attacking = true;
+	player = toAttack;
+}
+
+function Update(){
+	if (attacking && player != null){
+		transform.LookAt(player.transform);
+		//Lerp to a position just infront of the player
+		var newposition = player.transform.position;
+		newposition.z = player.transform.position.z + 100;
+		transform.position = Vector3.Lerp(transform.position, newposition, Time.deltaTime);
+	}
+}
