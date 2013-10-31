ceedd327bdc7d1fd7e3fef90dcd5b79b06fed5ea
Cameron Sutter <csutter@instructure.com>
2013-10-31 11:46:33 -0600

    plasma explosion prefab

--- Assets/shared/scripts/shieldBehavior.js
+++ Assets/shared/scripts/shieldBehavior.js
@@ -1,11 +1,13 @@
 ﻿#pragma strict
 
 public var duration : float = 0.5f;
+public var explosion : Transform;
 
 function OnCollisionEnter(col : Collision){
 	if(col.gameObject.layer == 12){
 		Destroy(col.gameObject);
 		//Add an explosion at the site of the collision
+		Instantiate(explosion, col.transform.position, col.transform.rotation);
 	}
 }
 
